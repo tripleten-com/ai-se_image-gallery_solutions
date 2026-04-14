77c4325881d70082907a931b74bf3efe6da9dd5a
Kevin Loughead <kvnloughead@gmail.com>
2026-04-13 21:17:31 -0400

    Make the modal module more reusable (option confirmation button)

--- 07-image-gallery-modal-factory/assets/js/modal.js
+++ 07-image-gallery-modal-factory/assets/js/modal.js
@@ -4,16 +4,18 @@ function generateModal(config) {
 
     function close() {
       config.modalEl.classList.remove(config.visibleClass);
-      config.confirmBtnEl.removeEventListener("click", handleConfirm);
+      config.confirmBtnEl &&
+        config.confirmBtnEl.removeEventListener("click", handleConfirm);
       config.cancelBtnEl.removeEventListener("click", close);
     }
 
     function handleConfirm() {
-      onConfirm();
+      onConfirm && onConfirm();
       close();
     }
 
-    config.confirmBtnEl.addEventListener("click", handleConfirm);
+    config.confirmBtnEl &&
+      config.confirmBtnEl.addEventListener("click", handleConfirm);
     config.cancelBtnEl.addEventListener("click", close);
   };
 }
